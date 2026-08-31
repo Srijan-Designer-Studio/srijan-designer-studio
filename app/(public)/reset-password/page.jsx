b3afc264e89791f2c7a716b76ad8a3d091296b09
Srijan-Designer-Studio <thedesignerstudio.srijan@gmail.com>
2026-09-01 00:08:17 +0530

    Update page.jsx

--- app/(public)/reset-password/page.jsx
+++ app/(public)/reset-password/page.jsx
@@ -45,7 +45,7 @@ export default function ResetPasswordPage() {
           password: password
         });
 
-     ে
+    
         if (error) {
           setMessage("Supabase Error: " + error.message);
           return;
